7dde19519ba206f8beffbd40e119402f98ca90ba
Shubham16598 <shubham16598@gmail.com>
2017-06-10 22:13:20 +0530

    Full Screen Feature Added

--- AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/SplashScreen.java
+++ AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/SplashScreen.java
@@ -2,12 +2,14 @@ package com.example.shubham16598.cricketmatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
     }
 }
