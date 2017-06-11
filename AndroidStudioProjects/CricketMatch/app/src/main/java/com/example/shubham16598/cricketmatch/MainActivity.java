d00d4ec2d79504855d60a0446f0a31ef149ca416
Shubham16598 <shubham16598@gmail.com>
2017-06-11 18:52:32 +0530

    Signup Activity

--- AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/MainActivity.java
+++ AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.shubham16598.cricketmatch;
 
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 }
