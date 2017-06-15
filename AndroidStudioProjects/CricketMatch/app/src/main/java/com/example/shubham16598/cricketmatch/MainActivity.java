49917da09508bab1ca31bbcec1d83ea032fe9bdb
Shubham16598 <shubham16598@gmail.com>
2017-06-15 22:46:32 +0530

    Firebase Added

--- AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/MainActivity.java
+++ AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.shubham16598.cricketmatch;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        Button b1 = (Button)findViewById(R.id.login);
+        Button b2 = (Button)findViewById(R.id.signup);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,signup.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/signup.java
+++ AndroidStudioProjects/CricketMatch/app/src/main/java/com/example/shubham16598/cricketmatch/signup.java
@@ -1,13 +1,14 @@
-package com.example.shubham16598.volleypractice;
+package com.example.shubham16598.cricketmatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
+
     }
 }
