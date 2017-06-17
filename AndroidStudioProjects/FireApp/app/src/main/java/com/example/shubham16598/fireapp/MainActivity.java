a7c51987eeab4175e9fe1ce47bfb9d6e2a8f93ff
Shubham16598 <shubham16598@gmail.com>
2017-06-17 10:31:50 +0530

    Complex Data adding method added

--- AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
+++ AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.shubham16598.fireapp;
 
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     public DatabaseReference mdatabase;
     Button b1;
     EditText e1;
+    EditText e2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +28,31 @@ public class MainActivity extends AppCompatActivity {
 
         b1= (Button)findViewById(R.id.addString);
         e1= (EditText)findViewById(R.id.editText);
+        e2=(EditText)findViewById(R.id.editText2);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s = e1.getText().toString().trim();
+                String name = e1.getText().toString().trim();
+                String email = e2.getText().toString().trim();
                 mdatabase = FirebaseDatabase.getInstance().getReference();
-                mdatabase.child("Name").setValue(s);
+
+                HashMap<String,String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name",name);
+                dataMap.put("Email",email);
+
+                mdatabase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this,"Sucessful",Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(MainActivity.this,"Unsuccesful",Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+
+
             }
         });
 
