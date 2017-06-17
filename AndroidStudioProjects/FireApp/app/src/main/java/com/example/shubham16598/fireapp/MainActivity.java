50437e365cc3e9547dbe8da06fcf22dddd7d3031
Shubham16598 <shubham16598@gmail.com>
2017-06-17 10:07:50 +0530

    Single Database added

--- AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
+++ AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
@@ -5,23 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     public DatabaseReference mdatabase;
-
+    Button b1;
+    EditText e1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b1;
+
         b1= (Button)findViewById(R.id.addString);
+        e1= (EditText)findViewById(R.id.editText);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String s = e1.getText().toString().trim();
                 mdatabase = FirebaseDatabase.getInstance().getReference();
+                mdatabase.child("Name").setValue(s);
             }
         });
 
