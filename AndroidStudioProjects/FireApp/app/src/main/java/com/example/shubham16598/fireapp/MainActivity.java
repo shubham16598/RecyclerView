fb311ff9bd6b08e6ada831ccdb26801bccd13dc4
Shubham16598 <shubham16598@gmail.com>
2017-06-17 09:41:54 +0530

    Realtime Database reference added

--- AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
+++ AndroidStudioProjects/FireApp/app/src/main/java/com/example/shubham16598/fireapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.shubham16598.fireapp;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    public DatabaseReference mdatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1;
+        b1= (Button)findViewById(R.id.addString);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mdatabase = FirebaseDatabase.getInstance().getReference();
+            }
+        });
+
     }
 }
